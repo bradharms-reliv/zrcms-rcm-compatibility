3df461805c3a49019aa7d1e19cf4623624e9e556
James Jervis <jjervis@relivinc.com>
2017-08-11 16:55:30 -0500

    Update

---
+++ ModulesConfig.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ZrcmsRcmCompatibility;
+/**
+ * @author James Jervis - https://github.com/jerv13
+ */
+class ModulesConfig
+{
+    /**
+     * __invoke
+     *
+     * @return array
+     */
+    public function __invoke()
+    {
+        $zrcmsModules = [
+            // Low level
+            new \ZrcmsRcmCompatibility\Rcm\ModuleConfig(),
+            new \ZrcmsRcmCompatibility\RcmDisabler\ModuleConfig(),
+        ];
+
+        $configManager = new \Zend\ConfigAggregator\ConfigAggregator(
+            $zrcmsModules
+        );
+
+        return $configManager->getMergedConfig();
+    }
+}
