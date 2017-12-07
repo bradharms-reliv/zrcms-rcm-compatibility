851d81b9caeb248053b5860289a5eaf5fa8188a1
James Jervis <jjervis@relivinc.com>
2017-12-06 18:27:20 -0600

    Update for refactor

---
+++ rcm-adapter/ComponentBlockRegistryBC.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace ZrcmsRcmCompatibility\RcmAdapter;
+
+use Zrcms\ContentCore\Block\Api\Component\ReadComponentConfigBlockBc;
+use Zrcms\ContentCore\Block\Api\Render\RenderBlockBc;
+use Zrcms\ContentCore\Block\Fields\FieldsBlockComponentConfig;
+
+/**
+ * @author James Jervis - https://github.com/jerv13
+ */
+class ComponentBlockRegistryBC
+{
+    const DEFAULT_COMPONENT_REGISTRY_KEY = 'zrcms-components';
+    /**
+     * @param array  $appConfig
+     * @param string $configFile I.E. __DIR__ . '/autoload/zrcms-blocks-bc.php'
+     * @param string $componentRegistryKey
+     *
+     * @return void
+     */
+    public static function buildConfigFile(
+        $appConfig,
+        string $configFile,
+        string $componentRegistryKey = self::DEFAULT_COMPONENT_REGISTRY_KEY
+    ) {
+        $bcConfigs = ComponentBlockRegistryBC::getBcConfigs(
+            $appConfig
+        );
+
+        $configFileContents = "<pre>"
+            . "<?php\n\n"
+            . "return [\n"
+            . "    '{$componentRegistryKey}' => \n"
+            . "    " . var_export($bcConfigs, true) . "\n"
+            . "];\n";
+
+        file_put_contents(
+            $configFile,
+            $configFileContents
+        );
+    }
+
+    /**
+     * @param array  $appConfig
+     * @param string $componentRegistryKey
+     *
+     * @return array
+     */
+    public static function buildAppConfig(
+        $appConfig,
+        string $componentRegistryKey = self::DEFAULT_COMPONENT_REGISTRY_KEY
+    ): array
+    {
+        if (!array_key_exists('rcmPlugin', $appConfig)) {
+            return $appConfig;
+        }
+
+        foreach ($appConfig['rcmPlugin'] as $rcmPluginName => $rcmPluginConfig) {
+            $appConfig[$componentRegistryKey]['block.' . $rcmPluginName] = ComponentBlockRegistryBC::getBcPluginConfig(
+                $rcmPluginName
+            );
+        }
+
+        return $appConfig;
+    }
+
+    /**
+     * @param array $appConfig
+     *
+     * @return array
+     */
+    public static function getBcConfigs(
+        $appConfig
+    ): array
+    {
+        if (!array_key_exists('rcmPlugin', $appConfig)) {
+            return [];
+        }
+
+        $componentRegistry = [];
+
+        foreach ($appConfig['rcmPlugin'] as $rcmPluginName => $rcmPluginConfig) {
+            $componentRegistry['block.' . $rcmPluginName] = ComponentBlockRegistryBC::getBcPluginConfig($rcmPluginName);
+        }
+
+        return $componentRegistry;
+    }
+
+    /**
+     * @param string $rcmPluginName
+     *
+     * @return array
+     */
+    public static function getBcPluginConfig(
+        string $rcmPluginName
+    ): array
+    {
+        $componentRegistryEntry = [];
+        $componentRegistryEntry[FieldsBlockComponentConfig::COMPONENT_CONFIG_READER]
+            = ReadComponentConfigBlockBc::SERVICE_ALIAS;
+        $componentRegistryEntry[FieldsBlockComponentConfig::CONFIG_LOCATION]
+            = $rcmPluginName;
+        $componentRegistryEntry[FieldsBlockComponentConfig::MODULE_DIRECTORY]
+            = "unknown";
+        $componentRegistryEntry[FieldsBlockComponentConfig::NAME]
+            = $rcmPluginName;
+        $componentRegistryEntry[FieldsBlockComponentConfig::RENDERER]
+            = RenderBlockBc::SERVICE_ALIAS;
+
+        return $componentRegistryEntry;
+    }
+}
